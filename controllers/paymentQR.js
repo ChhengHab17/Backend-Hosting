c9c4b32530794f2cb04da6ddc21fd9001bbef633
ChhengHab17 <chhenghab.te@student.cadt.edu.kh>
2025-09-12 16:14:29 +0700

    change url

--- controllers/paymentQR.js
+++ controllers/paymentQR.js
@@ -20,7 +20,7 @@ export const generateQr = async (req, res) => {
 
     // Call Python FastAPI microservice
     const pythonResponse = await axios.post(
-      "https://heather-blood-sheep-hypothesis.trycloudflare.com/generate-qr",
+      "https://discussing-dangerous-rate-focal.trycloudflare.com/generate-qr",
       payload
     );
 
@@ -48,7 +48,7 @@ export const verifyQr = async (req, res) => {
     const { md5 } = req.body;
 
     const response = await axios.post(
-      "https://heather-blood-sheep-hypothesis.trycloudflare.com/check-payment",
+      "https://discussing-dangerous-rate-focal.trycloudflare.com/check-payment",
       { md5 },
       { headers: { Authorization: `Bearer ${process.env.BAKONG_ACCESS_TOKEN}` } }
     );
